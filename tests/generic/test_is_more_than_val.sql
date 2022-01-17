c21736d825eba8c4a4c6bce3997329a5dfd43bdd
Nikita Bakanchev <bakanchevn@gmail.com>
2022-01-17 18:28:36 +0200

    add generalised test

---
+++ tests/generic/test_is_more_than_val.sql
@@ -0,0 +1,18 @@
+{% test is_more_than_val(model, column_name, value, strict=false) %}
+
+with validation as (
+    select {{ column_name }} as spec_field
+      from {{ model }}
+),
+validation_errors as (
+    select spec_field
+    from validation
+    {% if strict %}
+    where spec_field <= {{value}}
+    {% else %}
+    where spec_field < {{value}}
+    {% endif %}
+)
+select *
+from validation_errors
+{% endtest %}
